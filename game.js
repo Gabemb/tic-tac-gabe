7dc8182979767b1a1ff989fdceeb6d74d2568821
Gabemb <gabrielbny@gmail.com>
2017-12-16 17:12:50 -0500

    Add win animations
    Added win animations and keyframes to CSS file. Added end game logic to JS file to trigger the appropriate animations in the relevant grids on the game board.

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 const grid = document.querySelectorAll('.square');
 let turns = 0;
 
+// const dynamicCheckWin = (symbol, row, col) => {
+//   if (isTopRow(row)) {
+//     checkTopRow(symbol,row, col)
+//   }
+// }
 // Represenation of my game board
 const gameBoard = [
                     [null, null, null],
@@ -11,7 +16,17 @@ const isPlayerOneTurn = () => turns % 2 === 0;
 const isTopRow = (row) => row === 0;
 const isMidRow = (row) => row === 1;
 const isBottomRow = (row) => row === 2;
+const isXSymbol = (sym) => sym === 'x';
 
+
+const gridSquareAlreadyHasSymbol = (square) => square.innerHTML;
+
+/**
+ * checks if a win scenario has occurred yet
+ * @param  {string} symbol Going to be an 'x' or 'o'
+ * @param  {number} row   What row was clicked on
+ * @param  {number} col   What column was clicked on
+ */
 const checkForWin = (symbol, row, col) => {
   if (isTopRow(row)) {
     if (col === 0) {
@@ -48,25 +63,35 @@ const checkForWin = (symbol, row, col) => {
   }
 }
 
+const createArrayOfWinningElements = (...coordinates) => coordinates.map((coord) => document.getElementById(`${coord[0]}${coord[1]}`))
+const triggerXWinAnim = (elem) => elem.firstChild.classList.add('infinite-rotate-center');
+const triggerOWinAnim = (elem) => elem.firstChild.classList.add('infinite-bounce');
+
+const endGame = (symbol, coord1, coord2, coord3) => {
+  const elemArr = createArrayOfWinningElements(coord1, coord2, coord3);
+  if (isXSymbol(symbol)) {
+    elemArr.forEach(triggerXWinAnim);
+  } else {
+    elemArr.forEach(triggerOWinAnim);
+  }
+  console.log('you win!!!')
+}
+
 
 const checkTopLeft = (symbol, row, col) => {
   if (symbol === gameBoard[row][col+1]) {
     if (symbol === gameBoard[row][col+2]) {
-      console.log('you win!!!');
-      //endGame(symbol, )  <==== get back to this idjit
-      return;
+      return endGame(symbol, [row, col], [row, col+1], [row, col+2]);
     }
   }
   if (symbol === gameBoard[row+1][col+1]) {
     if (symbol === gameBoard[row+2][col+2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col+1], [row+2, col+2]);
     }
   }
   if (symbol === gameBoard[row+1][col]) {
     if (symbol === gameBoard[row+2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col], [row+2, col]);
     }
   }
 };
@@ -74,14 +99,12 @@ const checkTopLeft = (symbol, row, col) => {
 const checkTopMid = (symbol, row, col) => {
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col+1]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col+1]);
     }
   }
   if (symbol === gameBoard[row+1][col]) {
     if (symbol === gameBoard[row+2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col], [row+2, col]);
     }
   }
 };
@@ -89,20 +112,17 @@ const checkTopMid = (symbol, row, col) => {
 const checkTopRight = (symbol, row, col) => {
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col-2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col-2]);
     }
   }
   if (symbol === gameBoard[row+1][col-1]) {
     if (symbol === gameBoard[row+2][col-2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col-1], [row+2, col-2]);
     }
   }
   if (symbol === gameBoard[row+1][col]) {
     if (symbol === gameBoard[row+2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col], [row+2, col]);
     }
   }
 };
@@ -110,14 +130,12 @@ const checkTopRight = (symbol, row, col) => {
 const checkMidLeft = (symbol, row, col) => {
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row+1][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row+1, col]);
     }
   }
   if (symbol === gameBoard[row][col+1]) {
     if (symbol === gameBoard[row][col+2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col+1], [row, col+2]);
     }
   }
 };
@@ -125,26 +143,22 @@ const checkMidLeft = (symbol, row, col) => {
 const checkMidMid = (symbol, row, col) => {
   if (symbol === gameBoard[row-1][col-1]) {
     if (symbol === gameBoard[row+1][col+1]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col-1], [row+1, col+1]);
     }
   }
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col+1]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col+1]);
     }
   }
   if (symbol === gameBoard[row+1][col-1]) {
     if (symbol === gameBoard[row-1][col+1]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row+1, col-1], [row-1, col+1]);
     }
   }
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row+1][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row+1, col]);
     }
   }
 };
@@ -152,14 +166,12 @@ const checkMidMid = (symbol, row, col) => {
 const checkMidRight = (symbol, row, col) => {
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row+1][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row+1, col]);
     }
   }
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col-2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col-2]);
     }
   }
 };
@@ -167,20 +179,17 @@ const checkMidRight = (symbol, row, col) => {
 const checkBottomLeft = (symbol, row, col) => {
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row-2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row-2, col]);
     }
   }
   if (symbol === gameBoard[row-1][col+1]) {
     if (symbol === gameBoard[row-2][col+2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col+1], [row-2, col+2]);
     }
   }
   if (symbol === gameBoard[row][col+1]) {
     if (symbol === gameBoard[row][col+2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col+1], [row, col+2]);
     }
   }
 };
@@ -188,14 +197,12 @@ const checkBottomLeft = (symbol, row, col) => {
 const checkBottomMid = (symbol, row, col) => {
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col+1]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col+1]);
     }
   }
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row-2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row-2, col]);
     }
   }
 };
@@ -203,49 +210,49 @@ const checkBottomMid = (symbol, row, col) => {
 const checkBottomRight = (symbol, row, col) => {
   if (symbol === gameBoard[row][col-1]) {
     if (symbol === gameBoard[row][col-2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row, col-1], [row, col-2]);
     }
   }
   if (symbol === gameBoard[row-1][col-1]) {
     if (symbol === gameBoard[row-2][col-2]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col-1], [row-2, col-2]);
     }
   }
   if (symbol === gameBoard[row-1][col]) {
     if (symbol === gameBoard[row-2][col]) {
-      console.log('you win!!!');
-      return;
+      return endGame(symbol, [row, col], [row-1, col], [row-2, col]);
     }
   }
 };
 
 
-
+/**
+ * Makes the X divs spin around when clicked 
+ * OR 
+ * O divs wobble in place
+ */
 const triggerSymbolAnimation = (event) => {
   event.stopPropagation();
   const symbol = event.target;
-  const x = 'x';
-  if (symbol.className[0] === x) {
+  if (isXSymbol(symbol.className[0])) {
     // Removing entrance animation class to prevent it from triggering again
     symbol.classList.remove('rotating-scale-in-center');
     symbol.classList.add('rotate-center');
     // Removing the new animation class once symbol is done animating so it can be triggered again
     setTimeout(() => symbol.classList.remove('rotate-center'), 800);
-  } else {
+  } else { // if the symbol is an 'o' instead, trigger different animation
     symbol.classList.remove('bounce-in-top');
     symbol.classList.add('wobble');
     setTimeout(() => symbol.classList.remove('wobble'), 800);
   }
 };
 
+
 const insertSymbol = (event) => {
   event.stopPropagation();
   const gridSquare = event.target;
-  if (gridSquare.innerHTML) {
-    return;
-  }
+  if (gridSquareAlreadyHasSymbol(gridSquare)) return;
+
   const row = parseInt(event.target.id[0]);
   const col = parseInt(event.target.id[1]);
 
@@ -265,8 +272,12 @@ const insertSymbol = (event) => {
   gameBoard[row][col] = symbol;
 
   if (turns > 4) {
-    console.log('how many turns???', turns)
+    console.log(`
+      Turns: ${turns}
+      Game board: ${gameBoard}
+    `)
     checkForWin(symbol, row, col);
+    // dynamicCheckWin(symbol, row, col);
   }
 };
 
